9f5d2b4d8140d3625b7bca517e353f5c8b35c0ee
Ali Amirhamzeh and Jake Jensen <ali@>
2019-08-14 15:57:18 -0700

    clean the code up and changed the script link file

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,29 @@
+// This is the business (or back-end) logic:
 
-// Business (or back-end) logic:
-
-function add (number1, number2) {
+var add = function(number1, number2) {
   return number1 + number2;
 };
 
-// var subtract = function(number1, number2) {
-//   return number1 - number2;
-// };
-//
-// var multiply = function(number1, number2) {
-//   return number1 * number2;
-// };
-//
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// };
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-// User interface (or front-end) logic:
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Everything below this line is the user interface (or front-end) logic:
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
-    console.log("hi");
-   event.preventDefault()
+    event.preventDefault();
     var number1 = parseInt($("#add1").val());
-    console.log(number1);
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
     $("#output").text(result);
-    console.log(output);
   });
 });
